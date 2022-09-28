4736f9f0901ee51c24047b37105038a6ade24cb5
Nadia Lazarek <woj.dag@o2.pl>
2022-09-28 12:18:17 +0200

    deleted twig stuff from bad path of directories

--- tutorial/movies/src/Controller/MoviesController.php
+++ tutorial/movies/src/Controller/MoviesController.php
@@ -11,6 +11,8 @@ class MoviesController extends AbstractController
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('index.html.twig', [
+            'title' => 'Avengers: Endgame'
+        ]);
     }
 }
