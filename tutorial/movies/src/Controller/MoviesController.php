9735c3c1aa21a44b77d32868a63731268e38bf8c
Nadia Lazarek <woj.dag@o2.pl>
2022-10-05 15:31:24 +0200

    entity repositories - some methods with descriptions and examples

--- tutorial/movies/src/Controller/MoviesController.php
+++ tutorial/movies/src/Controller/MoviesController.php
@@ -20,8 +20,26 @@ class MoviesController extends AbstractController
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
+        // findAll() - SELECT * FROM movies;
+        // $movies = $repository->findAll();
+
+        // find() - SELECT * FROM movies WHERE id = 9;
+        // $movies = $repository->find(9);
+
+        // findBy() - SELECT * FROM movies ORDER BY id DESC;
+        // $movies = $repository->findBy([], ['id' => 'DESC']);
+
+        // findOneBy() - SELECT * FROM movies WHERE id = 6 AND title = 'The Dark Knight' ORDER BY id DESC
+        // $movies = $repository->findOneBy(['id' => 9, 'title' => 'The Dark Knight'], ['id' => 'DESC']);
+        // ^ both conditions in first array must be true if we want see a result
+        // ^^ second array is optional - we could say how we want sort the data
+
+        // count() - SELECT COUNT() FROM movies WHERE id = 9;
+        // $movies = $repository->count(['id' => 9]);
+        // ^ if we leave empty brackets [] in count method, we will get a result of all counted rows is database
+
         $repository = $this->em->getRepository(Movie::class);
-        $movies = $repository->findAll();
+        $movies = $repository->getClassName();
         dd($movies);
 
         return $this->render('index2.html.twig');
